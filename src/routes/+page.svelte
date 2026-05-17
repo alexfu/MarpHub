3e3d8dd9e2b677cd0959799c9638b957e3e6986c
Alex Fu <alexfu@fastmail.com>
2026-05-17 15:45:28 -0400

    clean up main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
 <form method="POST" enctype="multipart/form-data">
   <input name="title" type="text" required />
   <input name="presentation" type="file" accept=".md,.markdown,text/markdown" required />
