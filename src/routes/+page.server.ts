f819bcb14ead57da8628cb7d5910aa696d56785a
Alex Fu <alexfu@fastmail.com>
2026-05-17 13:17:44 -0400

    basic form upload for creating presentation record

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,29 @@
+import { prisma } from '$lib/db';
+import { redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+
+export const actions = {
+	default: async ({ request }) => {
+		const formData = await request.formData();
+
+		const presentationFile = formData.get('presentation') as File;
+		const presentationContent = await presentationFile.text();
+		const presentation = await prisma.presentation.create({
+			data: {
+				content: presentationContent,
+				user: {
+					connectOrCreate: {
+						create: {
+							username: 'alexfu'
+						},
+						where: {
+							username: 'alexfu'
+						}
+					}
+				}
+			}
+		});
+
+		return redirect(302, `/presentations/${presentation.id}`);
+	}
+} satisfies Actions;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<form method="POST" enctype="multipart/form-data">
+	<input name="presentation" type="file" accept=".md,.markdown,text/markdown" />
+	<button>Submit</button>
+</form>
