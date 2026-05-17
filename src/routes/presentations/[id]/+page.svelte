48759e653c10ccd29e1149605ea54a9589ded98c
Alex Fu <alexfu@fastmail.com>
2026-05-17 14:18:05 -0400

    render presentation

--- src/routes/presentations/[id]/+page.svelte
+++ src/routes/presentations/[id]/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+	import { Marp } from '@marp-team/marp-core';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
+	let rendered = $derived.by(() => {
+		return new Marp().render(data.presentation.content);
+	});
 </script>
+
+{@html rendered.html}
+<svelte:head>{@html `<style>${rendered.css}</style>`}</svelte:head>
