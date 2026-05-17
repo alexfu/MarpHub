0cd154d51dc840f4de420a3b09a047d92f3dc7c0
Alex Fu <alexfu@fastmail.com>
2026-05-17 15:43:20 -0400

    add listing route

---
+++ src/routes/presentations/+page.server.ts
@@ -0,0 +1,7 @@
+import { prisma } from '$lib/db';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => {
+	const presentations = await prisma.presentation.findMany();
+	return { presentations };
+};
---
+++ src/routes/presentations/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+
+	let { data } = $props();
+</script>
+
+<ul>
+	{#each data.presentations as presentation (presentation.id)}
+		<li><a href={resolve(`/presentations/${presentation.id}`)}>{presentation.title}</a></li>
+	{/each}
+</ul>
