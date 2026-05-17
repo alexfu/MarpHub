2723ef66cf08b43bf79e9d179eda34a00a3b1f66
Alex Fu <alexfu@fastmail.com>
2026-05-17 13:49:29 -0400

    load presentation

--- src/routes/presentations/[id]/+page.server.ts
+++ src/routes/presentations/[id]/+page.server.ts
@@ -1,6 +1,11 @@
+import { prisma } from '$lib/db';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = () => {
-	console.log('hi from +page.server.ts');
-	return {};
+export const load: PageServerLoad = async ({ params }) => {
+	const presentation = await prisma.presentation.findFirst({ where: { id: params.id } });
+	if (!presentation) {
+		return error(404, 'Not found');
+	}
+	return { presentation };
 };
