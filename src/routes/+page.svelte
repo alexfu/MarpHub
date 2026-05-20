2bd21de7b5d7c2294836fa8c243c3c9f508d92c8
Alex Fu <alexfu@fastmail.com>
2026-05-20 08:23:54 -0400

    remove upload form from home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,3 @@
     <li><a href={resolve(`/presentations/${presentation.id}`)}>{presentation.title}</a></li>
   {/each}
 </ul>
-
-<form method="POST" enctype="multipart/form-data">
-  <input name="title" type="text" required />
-  <input name="presentation" type="file" accept=".md,.markdown,text/markdown" required />
-  <button>Submit</button>
-</form>
