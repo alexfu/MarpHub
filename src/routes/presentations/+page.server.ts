4815311984be749c0f9687d9d1f733cd023501ac
Alex Fu <alexfu@fastmail.com>
2026-05-21 14:03:09 -0400

    delete presentation listing page

--- src/routes/presentations/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { prisma } from '$lib/db';
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async () => {
-  const presentations = await prisma.presentation.findMany();
-  return { presentations };
-};
