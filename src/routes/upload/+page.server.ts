b1c2ce89914169ff20b826cc809fad064412bd44
Alex Fu <alexfu@fastmail.com>
2026-05-21 10:45:48 -0400

    implement upload

--- src/routes/upload/+page.server.ts
+++ src/routes/upload/+page.server.ts
@@ -1,10 +1,32 @@
 import { auth } from '$lib/auth';
 import { signInRedirect } from '$lib/auth/utils';
+import { prisma } from '$lib/db.js';
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { resolve } from '$app/paths';
 
 export const load: PageServerLoad = async ({ request }) => {
   const session = await auth.api.getSession({ headers: request.headers });
   if (!session) {
     return signInRedirect(request);
+  }
+};
+
+export const actions = {
+  default: async ({ request }) => {
+    const session = await auth.api.getSession({ headers: request.headers });
+    const formData = await request.formData();
+    const presentation = await prisma.presentation.create({
+      data: {
+        title: formData.get('title') as string,
+        content: await (formData.get('file') as File).text(),
+        user: {
+          connect: {
+            id: session?.user.id
+          }
+        }
+      }
+    });
+    return redirect(302, resolve(`/presentations/${presentation.id}`));
   }
 };
--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,8 +1,22 @@
-<div class="container">
-  <form>
+<div class="container-sm">
+  <h1 class="page-title">New presentation</h1>
+  <form method="POST" enctype="multipart/form-data">
     <div class="input-container">
       <label for="title">Title</label>
       <input name="title" type="text" required />
     </div>
+    <div class="input-container">
+      <label for="file">Markdown File</label>
+      <input name="file" type="file" accept=".md,.markdown,text/markdown" required />
+    </div>
+    <button class="btn primary full">Create presentation</button>
   </form>
 </div>
+
+<style>
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+  }
+</style>
