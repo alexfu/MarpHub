a791833bec6977ada13122382f877b8072535058
Alex Fu <alexfu@fastmail.com>
2026-05-21 11:12:47 -0400

    clean up

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -2,24 +2,13 @@
   let { children } = $props();
 </script>
 
-<div class="container">
-  <div class="card auth">
+<div class="container-sm">
+  <div class="card">
     {@render children()}
   </div>
 </div>
 
 <style>
-  .container {
-    padding: 24px;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-  }
-
-  .auth {
-    width: 360px;
-  }
-
   .card {
     background: var(--surface);
     border: 1px solid var(--border);
